da1340054cf6e7c1b6ab9d883600a409e3d75d5d
John Adebayo <johnayomide31@gmail.com>
2025-09-02 23:37:32 +0100

    Add layout and partials for main application structure
    - Created layout.ejs for the main HTML structure including head, header, navigation, and footer.
    - Added head.ejs for meta tags and stylesheet link.
    - Implemented header.ejs for site branding and account link.
    - Developed navigation.ejs for main navigation links.
    - Created footer.ejs to display copyright information with the current year.

--- server.js
+++ server.js
@@ -6,9 +6,18 @@
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+
+/* ***********************
+ * View Engine and Template
+ *************************/
+
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "layouts/layout")
 
 /* ***********************
  * Routes
@@ -20,11 +29,11 @@ app.use(static)
  * Values from .env (environment) file
  *************************/
 const port = process.env.PORT
-const host = process.env.HOST
+// const host = process.env.HOST
 
 /* ***********************
  * Log statement to confirm server operation
  *************************/
 app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`)
+  console.log(`app listening on port:${port}`)
 })
